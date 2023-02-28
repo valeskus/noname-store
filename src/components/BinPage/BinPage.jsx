ea55088a8a8cb096285cbd1604b4ec318b6bda9e
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-02-28 17:10:30 +0200

    Added pages components

---
+++ src/components/BinPage/BinPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function BinPage() {
+	return <div></div>;
+}
---
+++ src/components/Registration/Registration.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Registration() {
+	return <div></div>;
+}
