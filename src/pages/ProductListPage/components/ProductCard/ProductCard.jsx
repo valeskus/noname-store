d625f833e0a6a865a3053bea42176d497d579a10
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-03-01 22:17:14 +0200

    fixed style

--- src/pages/ProductListPage/components/ProductCard/ProductCard.jsx
+++ src/pages/ProductListPage/components/ProductCard/ProductCard.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Button } from '../../../../components/common/Button';
 import './ProductCard.style.css';
 
 export function ProductCard({ product }) {
+	const randomImageId = Math.floor(Math.random() * 200);
 	return (
 		<div className='productCard-container'>
-			<div>
+			<img
+				className='card-image'
+				src={`https://picsum.photos/id/${randomImageId}/200`}
+				alt='logo'
+			/>
+			<div className='productCard-content'>
 				<h3>{product.title}</h3>
-				<p>{product.description}</p>
+			</div>
+			<div className='productCard-button'>
+				<Button> BUY</Button>
 			</div>
 		</div>
 	);
